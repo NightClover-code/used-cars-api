140b56f63b5b9b47100925011b1cbe29c5d5a6ff
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-24 00:23:23 +0100

    initializing sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { User } from './users/users.entity';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
   ],
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
+import { Report } from './reports.entity';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [Report],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
