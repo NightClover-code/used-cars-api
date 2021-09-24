4d777d8ad1e419da33db9023b96d00da5580f9c0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-24 02:34:00 +0100

    handled route exeptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
@@ -13,8 +13,12 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  findOne(id: number) {
-    return this.repo.findOne(id);
+  async findOne(id: number) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) throw new NotFoundException('user not found');
+
+    return user;
   }
 
   find(email: string) {
@@ -24,7 +28,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
 
-    if (!user) throw new Error('user not found');
+    if (!user) throw new NotFoundException('user not found');
     Object.assign(user, attrs);
 
     return this.repo.save(user);
@@ -33,7 +37,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
 
-    if (!user) throw new Error('user not found');
+    if (!user) throw new NotFoundException('user not found');
 
     return this.repo.remove(user);
   }
