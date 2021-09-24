dee55fe4257777e432f5b220baf9f3263a9ef4e6
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-24 01:38:40 +0100

    completed user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,7 +21,20 @@ export class UsersService {
     return this.repo.find({ email });
   }
 
-  update() {}
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
 
-  remove() {}
+    if (!user) throw new Error('user not found');
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) throw new Error('user not found');
+
+    return this.repo.remove(user);
+  }
 }
