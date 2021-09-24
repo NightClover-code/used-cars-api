9033b9e71b91bc2e950e6c975cec841d9826e4ba
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-24 02:13:00 +0100

    adding request handlers complete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,11 @@ import {
   Get,
   Param,
   Patch,
+  Delete,
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -15,8 +17,8 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+  createUser(@Body() { email, password }: CreateUserDto) {
+    return this.usersService.create(email, password);
   }
 
   @Get('/:id')
@@ -27,5 +29,15 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
