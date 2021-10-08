8f490c3dd26e4aeb785eed99c94bcff8cd3b5371
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-10-08 21:17:43 +0100

    introduction to unit testing

---
+++ src/users/services/tests/auth.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { User } from 'src/users/entities';
+import { AuthService } from '..';
+import { UsersService } from '..';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+});
