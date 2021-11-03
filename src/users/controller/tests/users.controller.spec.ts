7b05831ccbe393f1478f710b197061e845417527
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-03 02:45:17 +0100

    testing controller, not a lot to test

--- src/users/controller/tests/users.controller.spec.ts
+++ src/users/controller/tests/users.controller.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../users.controller';
+import { UsersService, AuthService } from '../../services';
+import { User } from '../../entities';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'adoacsn@gmail.com',
+          password: '123',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            email: 'adoacsn@gmail.com',
+            password: '49484',
+          } as User,
+        ]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+
+    fakeAuthService = {
+      // signin: () => {},
+      // signup: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: fakeUsersService },
+        { provide: AuthService, useValue: fakeAuthService },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +48,12 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('it returns a list of users with given email', async () => {
+    const users = await controller.findAllUsers('adoacsn@gmail.com');
+
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('adoacsn@gmail.com');
   });
 });
