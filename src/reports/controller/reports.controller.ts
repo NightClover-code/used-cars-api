018f7e0f8298e3b51c88d6c416a83e3b061caac0
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-07 00:53:55 +0100

    adding a create report dto

--- src/reports/controller/reports.controller.ts
+++ src/reports/controller/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, UseGuards } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { AuthGuard } from 'src/guards';
+import { CreateReportDto } from '../dtos';
+import { ReportsService } from '../services';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {}
+}
