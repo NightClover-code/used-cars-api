7c6f6a810107a95055dff2939877a7a79ad43f2a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-07 01:08:50 +0100

    testing report creation

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  (app as any).set('etag', false);
-  app.use((req, res, next) => {
-    res.removeHeader('x-powered-by');
-    res.removeHeader('date');
-    next();
-  });
+
   await app.listen(3000);
 }
 bootstrap();
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportsController } from './controller';
 import { Report } from './entities';
 import { ReportsService } from './services';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [Report],
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
