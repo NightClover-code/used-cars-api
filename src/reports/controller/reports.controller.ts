9b43ad83650cb24fbbd19153b38334b32c10747d
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-11 04:12:06 +0100

    transforming properties with a dto

--- src/reports/controller/reports.controller.ts
+++ src/reports/controller/reports.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, UseGuards } from '@nestjs/common';
 import { Post } from '@nestjs/common';
+import { CurrentUser } from 'src/decorators';
 import { AuthGuard } from 'src/guards';
+import { Serialize } from 'src/interceptors';
+import { User } from 'src/users/entities';
 import { CreateReportDto } from '../dtos';
+import { ReportDto } from '../dtos/report.dto';
 import { ReportsService } from '../services';
 
 @Controller('reports')
@@ -10,7 +14,8 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
