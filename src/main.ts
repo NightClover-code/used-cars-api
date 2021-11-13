7e7f9bee0f5d18fd3ab68841396abe0bb05d5692
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-13 13:43:29 +0100

    will add swagger documentation later

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Used cars API')
+    .setDescription('a RESTful API')
+    .setVersion('1.0.0')
+    .addTag('used-cars')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
