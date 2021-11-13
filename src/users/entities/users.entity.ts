e3ec44ea8777ddf7b4e150df92bd8feb7738eb85
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-11-13 13:28:35 +0100

    removing admin for everyone

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -20,7 +20,7 @@ export class User {
   @Column()
   password: string;
 
-  @Column({ default: true })
+  @Column({ default: false })
   admin: boolean;
 
   @OneToMany(() => Report, report => report.user)
